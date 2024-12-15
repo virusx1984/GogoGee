ae5a6d04df4f888382ed217529e78e961ce71d5e
Lining <virusx1984@gmail.com>
2024-12-15 22:05:38 +0800

    update some ui file and test some models

--- flask_restful_api/models.py
+++ flask_restful_api/models.py
@@ -10,3 +10,12 @@ class Task(db.Model):
 
     def __repr__(self):
         return f'<Task {self.description}>'
+    
+
+class Task2(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(100), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<Task {self.description}>'
